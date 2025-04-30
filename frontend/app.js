a59c62b6fc88b617cc50ef23637ca2e75d43fa7c
PriyaMaan <84952481+PriyaMaan@users.noreply.github.com>
2025-04-30 13:43:47 +0530

    Delete frontend directory

--- frontend/app.js
+++
@@ -1,6 +0,0 @@
-async function submitGuess() {  
-    const guess = document.getElementById("guessInput").value;  
-    const response = await fetch(`/guess/Rock/${guess}`, { method: "POST" });  
-    const result = await response.json();  
-    alert(result.message);  
-}  
